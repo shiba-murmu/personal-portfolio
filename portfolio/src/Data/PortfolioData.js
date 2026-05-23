c563d41fe0848d1d022092117f043d60c20e4fb8
Shiba Murmu <shibamurmu001@gmail.com>
2026-05-23 15:09:38 +0530

    portfolio data is added to the page is it universal data page.

---
+++ portfolio/src/Data/PortfolioData.js
@@ -0,0 +1,19 @@
+// Hero section data
+
+const PortfolioData = {
+    herosection: {
+        name : 'Shiba Murmu',
+        passion : 'DEVELOPER PORTFOLIO',
+        role : 'Full Stack Developer',
+        stack : ['React', 'Django', 'AI'],
+        projectsBuilt : '10+',
+        modernFrontend : 'React',
+        paragraph : ' I create modern, scalable and visually immersive web applications with React, Django and AI-powered technologies focused on performance and user experience.',
+        links : {
+            linkedin : 'https://linkedin.com/in/shibamurmu',
+            github : 'https://github.com/shiba-murmu'
+        }
+    }
+}
+
+export default PortfolioData
